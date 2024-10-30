6484edb591aab1304b93b2fe0e29b1a8f8f8140d
Michael <Michaelpage05@hotmail.co.uk>
2024-10-30 23:15:45 +0000

    added filament

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasName;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class Member extends Authenticatable
+class Member extends Authenticatable implements FilamentUser, HasName
 {
     use HasFactory, Notifiable, HasUuids;
 
@@ -45,5 +48,15 @@ class Member extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+
+    public function getFilamentName(): string
+    {
+        return $this->first_name;
     }
 }
