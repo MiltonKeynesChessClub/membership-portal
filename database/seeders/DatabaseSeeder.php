1f4eda14c252b985e2eef709c9acaf9c285d042f
Michael <Michaelpage05@hotmail.co.uk>
2024-10-30 21:25:27 +0000

    init commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Member;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Member::factory(10)->create();
+
+        Member::factory()->create([
+            'name' => 'Test Member',
+            'email' => 'test@example.com',
+        ]);
+    }
+}
